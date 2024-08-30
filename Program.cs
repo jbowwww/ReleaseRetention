d2634e21e2a103e58a26b912076b1b07343db145
Joel Bowman <jbowwww@gmail.com>
2024-08-31 04:47:05 +0800

    Deserializes Project array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var projects = JsonSerializer.Deserialize<Project[]>(JsonDocument.Parse(File.ReadAllBytes("./Projects.json")));
+Console.WriteLine($"Projects ({projects?.Length}): {string.Join("\n\t", projects?.Select(p => $"Project: Id=\"{p.Id}\" Name=\"{p.Name}\""))}");
+
+class Project
+{
+    [JsonInclude]
+    public string Id;
+    [JsonInclude]
+    public string Name;
+}
