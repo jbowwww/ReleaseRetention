93c77935141a061f52c1945b9faa4f5e844336ec
Joel Bowman <jbowwww@gmail.com>
2024-09-02 12:58:31 +0800

    Remove unused generic type constraint on IEnumerable_Extensions.ToString

--- IEnumerable_Extensions.cs
+++ IEnumerable_Extensions.cs
@@ -1,6 +1,5 @@
 public static class IEnumerable_Extensions
 {
-    public static string ToString<T>(this IEnumerable<T> strings, string name = "[Enumerable]")
-        /* where T : class */ =>
+    public static string ToString<T>(this IEnumerable<T> strings, string name = "[Enumerable]") =>
         $"\n\n{name} ({strings?.Count()}):\n\t{string.Join("\n\t", strings ?? [])}";
 }
