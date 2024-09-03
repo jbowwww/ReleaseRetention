d1aa335e05b8024826a05dd96a359afaa79d5979
Joel Bowman <jbowwww@gmail.com>
2024-09-03 19:35:56 +0800

    Add Entity construction tests

--- ReleaseRetention.Tests/EntityTests.cs
+++ ReleaseRetention.Tests/EntityTests.cs
@@ -3,8 +3,90 @@ namespace ReleaseRetention.Tests;
 public class EntityTests
 {
     [Fact]
-    public void Test1()
+    [Trait("Category", "Entity")]
+    [Trait("EntityType", nameof(Project))]
+    [Trait("Category", "Construction")]
+    public void Project_Construction_SetsPropertiesAndReturnsInstance()
     {
-        Assert.True(true);
+        var expected = new
+        {
+            Id = "Id-1",
+            Name = "Name-1"
+        };
+        var actual = new Project()
+        {
+            Id = expected.Id,
+            Name = expected.Name
+        };
+        Assert.IsType<Project>(actual);
+        Assert.Equivalent(expected, actual, false);
+    }
+    
+    [Fact]
+    [Trait("Category", "Entity")]
+    [Trait("EntityType", nameof(Environment))]
+    [Trait("Category", "Construction")]
+    public void Environment_Construction_SetsPropertiesAndReturnsInstance()
+    {
+        var expected = new
+        {
+            Id = "Id-1",
+            Name = "Name-1"
+        };
+        var actual = new Environment()
+        {
+            Id = expected.Id,
+            Name = expected.Name
+        };
+        Assert.IsType<Environment>(actual);
+        Assert.Equivalent(expected, actual, true);
+    }
+
+    [Fact]
+    [Trait("Category", "Entity")]
+    [Trait("EntityType", nameof(Release))]
+    [Trait("Category", "Construction")]
+    public void Release_Construction_SetsPropertiesAndReturnsInstance()
+    {
+        var expected = new
+        {
+            Id = "Id-1",
+            ProjectId = "Project-Id-1",
+            Version = "1.0.1",
+            Created = new DateTime(1996, 12, 25, 11, 08, 01),
+        };
+        var actual = new Release()
+        {
+            Id = "Id-1",
+            ProjectId = "Project-Id-1",
+            Version = "1.0.1",
+            Created = new DateTime(1996, 12, 25, 11, 08, 01),
+        };
+        Assert.IsType<Release>(actual);
+        Assert.Equivalent(expected, actual, false);
+    }
+
+    [Fact]
+    [Trait("Category", "Entity")]
+    [Trait("EntityType", nameof(Deployment))]
+    [Trait("Category", "Construction")]
+    public void Deployment_Construction_SetsPropertiesAndReturnsInstance()
+    {
+        var expected = new
+        {
+            Id = "Id-1",
+            ReleaseId = "Release-Id-1",
+            EnvironmentId = "Environment-Id-1",
+            DeployedAt = new DateTime(1998, 4, 19, 03, 48, 08),
+        };
+        var actual = new Deployment()
+        {
+            Id = "Id-1",
+            ReleaseId = "Release-Id-1",
+            EnvironmentId = "Environment-Id-1",
+            DeployedAt = new DateTime(1998, 4, 19, 03, 48, 08),
+        };
+        Assert.IsType<Deployment>(actual);
+        Assert.Equivalent(expected, actual, false);
     }
 }
