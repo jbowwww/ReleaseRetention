c447e36457ff70ec4d8fc566447fa90974d88a9d
Joel Bowman <jbowwww@gmail.com>
2024-09-05 16:48:27 +0800

    Add DataContext tests

---
+++ ReleaseRetention.Tests/Data/Context/DataContext.cs
@@ -0,0 +1,64 @@
+using Data.Entities;
+using Environment = Data.Entities.Environment;
+
+namespace ReleaseRetention.Tests.DataContext;
+
+internal class MyDataContext : Data.Context.DataContext
+{
+    internal MyDataContext(
+        IEnumerable<Project> projects,
+        IEnumerable<Environment> environments,
+        IEnumerable<Release> releases,
+        IEnumerable<Deployment> deployments
+    ) {
+        Projects = projects;
+        Environments = environments;
+        Releases = releases;
+        Deployments = deployments;
+    }
+}
+
+public class DataContext_Should
+{
+    [Fact]
+    [Trait("Category", "DataContext")]
+    [Trait("Type", nameof(DataContext))]
+    [Trait("Category", "Construction")]
+    public void DataContext_Construction_LoadsData()
+    {
+        var expected = new {
+            Projects = new []
+            {
+                new Project() { Id = "P1", Name = "Project 1", },
+                new Project() { Id = "P2", Name = "Project 2", },
+            },
+            Environments = new []
+            {
+                new Environment() { Id = "E1", Name = "Environment 1", },
+                new Environment() { Id = "E2", Name = "Environment 2", },
+            },
+            Releases = new []
+            {
+                new Release() { Id = "R1", Version = "1.0.0", ProjectId = "P1", Created = DateTime.Parse("2020-03-08T09:00:00"), },
+                new Release() { Id = "R2", Version = "1.0.1", ProjectId = "P1", Created = DateTime.Parse("2020-03-08T09:30:00"), },
+                new Release() { Id = "R3", Version = "1.0.0", ProjectId = "P2", Created = DateTime.Parse("2020-03-08T08:30:00"), },
+                new Release() { Id = "R4", Version = "1.0.1", ProjectId = "P2", Created = DateTime.Parse("2020-03-08T08:45:00"), },
+            },
+            Deployments = new []
+            {
+                new Deployment() { Id = "D1", ReleaseId = "R1", EnvironmentId = "E1", DeployedAt = DateTime.Parse("2020-03-08T10:00:00"), },
+                new Deployment() { Id = "D2", ReleaseId = "R1", EnvironmentId = "E2", DeployedAt = DateTime.Parse("2020-03-08T10:10:00"), },
+                new Deployment() { Id = "D3", ReleaseId = "R4", EnvironmentId = "E2", DeployedAt = DateTime.Parse("2020-03-08T09:30:00"), },
+                new Deployment() { Id = "D4", ReleaseId = "R3", EnvironmentId = "E2", DeployedAt = DateTime.Parse("2020-03-08T10:15:00"), },
+                new Deployment() { Id = "D1", ReleaseId = "R3", EnvironmentId = "E1", DeployedAt = DateTime.Parse("2020-03-08T10:50:00"), },
+            },
+        };
+
+        var myDataContext = new MyDataContext(expected.Projects, expected.Environments, expected.Releases, expected.Deployments);
+        
+        Assert.IsType<MyDataContext>(myDataContext);
+        Assert.Distinct(myDataContext.Projects);
+        Assert.Equal(myDataContext.Projects.Count(), expected.Projects.Length);
+        Assert.All(myDataContext.Projects, p => Assert.IsType<Project>(p));
+    }
+}
